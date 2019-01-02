7ede7f4de7fd1bf62d99d61f0800895dc7812bee
Master-Alcy <aijingxuan92@gmail.com>
2019-01-02 17:53:45 +0800

    done two, need to optimal second one

---
+++ Preparations/Docs/LeetCode_Diary.md
@@ -0,0 +1,6 @@
+# Start this Diary on 2019/1/2
+
+## 2019/1/2
+
+* 1 - Two Sum - Easy - HashMap's .containsKey, .get and .put / Rest is just iterating through
+* 2 - Add Two Numbers - Medium - Achieved first acceptance, though slow. Implemented with StringBuilder, BigInteger and String[].
--- Preparations/LeetCode/hashmap/TwoSum.java
+++ Preparations/LeetCode/hashmap/TwoSum.java
@@ -4,22 +4,28 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class twosum {
+public class TwoSum {
 
 	public static void main(String[] args) {
-		twosum t = new twosum();
-		int[] a = { 3, 2, 4 };
-		int[] result = t.twoSum(a, 6);
+		TwoSum t = new TwoSum();
+		int[] a = { 2, 7, 11, 15 };
+		int[] result = t.twoSum(a, 9);
 		System.out.println(Arrays.toString(result));
 	}
 
 	public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
-
+		
+		// Iterating through array integers
 		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(target - nums[i])) {
-				return new int[] { map.get(target - nums[i]), i };
+			
+			// This is the number we need to find
+            int numNeed = target - nums[i];
+			if (map.containsKey(numNeed)) {
+				return new int[] { map.get(numNeed), i };
 			}
+			
+			// First number being put into Map here
 			map.put(nums[i], i);
 		}
 		return new int[] { 0, 0 };
