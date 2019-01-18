972a0fbff6c81cd26c3b0e0c49f925458fd09882
Master-Alcy <aijingxuan92@gmail.com>
2019-01-18 16:31:40 +0800

    add

---
+++ Preparations/LeetCode/greedy/AssignCookies.java
@@ -0,0 +1,14 @@
+package greedy;
+
+public class AssignCookies {
+
+    public static void main(String[] args) {
+        AssignCookies ac = new AssignCookies();
+
+    }
+
+    private int findContentChildren(int[] g, int[] s) {
+
+        return 0;
+    }
+}
