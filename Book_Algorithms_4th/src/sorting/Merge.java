f1cd731a663053f7b0014a7c8b33580056f86ac1
Master-Alcy <aijingxuan92@gmail.com>
2019-03-02 10:13:54 -0500

    add

--- Book_Algorithms_4th/src/sorting/Merge.java
+++ Book_Algorithms_4th/src/sorting/Merge.java
@@ -19,8 +19,9 @@ public class Merge {
 	}
 
 	private static void sort(Comparable[] a, int lo, int hi) { // Sort a[lo..hi].
-		if (hi <= lo)
+		if (hi <= lo) {
 			return;
+		} // hi > lo
 		int mid = lo + (hi - lo) / 2;
 		sort(a, lo, mid); // Sort left half.
 		sort(a, mid + 1, hi); // Sort right half.
