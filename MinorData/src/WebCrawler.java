526b1f5576ee6fc27a160f96c8469667fa0ae752
Master-Alcy <aijingxuan92@gmail.com>
2019-03-08 16:51:50 -0500

    add

--- MinorData/src/WebCrawler.java
+++ MinorData/src/WebCrawler.java
@@ -1,8 +1,13 @@
 package com.riskval.webcrawler;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +16,7 @@ import java.util.Properties;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import com.riskval.app.ApplicationLog;
 import com.riskval.app.LocalSettings;
@@ -19,15 +25,55 @@ public class WebCrawler {
 
 	public static void main(String[] args) {
 		WebCrawler wc = new WebCrawler();
-		List<List<HashMap<String, String>>> mapResult = wc.processWebcrawler();
-		wc.printHashMapData(mapResult);
+		wc.printHashMapData(wc.processWebcrawler());
 		
-//		test();
+//		wc.getCompanyLogo("Apple_Inc.");
+//		wc.getCompanyLogo("Google");
 	}
 	
-	private static void test() {
+	private void symbolToCompany(String symbol) {
+		switch (symbol) {
+			case "AAPL":
+				getCompanyLogo("Apple_Inc.");
+				return;
+			case "GOOG":
+				getCompanyLogo("Google");
+				return;
+			default:
+				System.out.println("Not supported");
+		}
+	}
+	
+	private void getCompanyLogo(String company) {
 //		String test = wc.readSymbol2("https://www.google.com/search?num=10&site=finance&tbm=fin&q=NASDAQ:+AAPL#scso=_UJaCXJKhA8Hj_Aaxt5ngDQ2:0");
 //		System.out.println(test);
+		try {
+			Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + company).get();
+			Element img = doc.select("td.logo").get(0).getElementsByTag("img").get(0);
+			String src = img.absUrl("src");
+			System.out.println("Image Found! Its src is: " + src);
+			getLogo(src, company);
+			return;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Image not found...");
+	}
+
+	private void getLogo(String src, String name) {
+		try {
+			URL url = new URL(src);
+			InputStream in = url.openStream();
+			// C:\riskval\Client\config
+			OutputStream out = new BufferedOutputStream(new FileOutputStream("C:\\riskval\\Client\\config\\" + name + ".png"));
+			for (int b; (b = in.read()) != -1;) {
+				out.write(b);
+			}
+			out.close();
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -56,6 +102,7 @@ public class WebCrawler {
 				String anchor = oneWeb[4];
 
 				for (int j = 0; j < symbols.length; j++) {
+					symbolToCompany(symbols[j]);
 					oneList.add(getData(readSymbol(queryURL + symbols[j]), anchor, fields));
 				}
 			} else if (oneWeb[0].equals("webcrawler.properties.morningstar")) {
@@ -66,6 +113,7 @@ public class WebCrawler {
 				String anchor = oneWeb[4];
 
 				for (int j = 0; j < symbols.length; j++) {
+					symbolToCompany(symbols[j]);
 					oneList.add(getData(readSymbol(queryURL + symbols[j].toLowerCase() + "/quote.html"), anchor, fields));
 				}
 			} // end of if-else if
