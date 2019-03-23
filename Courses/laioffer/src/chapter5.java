17026f1ca96631e8fe159b9d77e6cdb4abf9e7fc
Master-Alcy <aijingxuan92@gmail.com>
2019-03-23 10:06:50 -0400

    add

---
+++ Courses/laioffer/src/chapter5.java
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class chapter5 {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.offer(1);
+        pq.offer(2);
+        pq.offer(3);
+
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+    }
+}
---
+++ Courses/laioffer/src/chapter6.java
@@ -0,0 +1,2 @@
+public class chapter6 {
+}
